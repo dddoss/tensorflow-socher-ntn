a6545023c4b2e62c3112454ae472b533819c74d2
Alexander Lenail <alexander.lenail@tufts.edu>
2015-12-05 18:38:26 -0500

    added some idea about how to perform the forward step

--- ntn.py
+++ ntn.py
@@ -1,14 +1,33 @@
 import tensorflow as tf
 
 
-# tf.tanh()
+
+# e1 and e2 are d-dimensional entity vectors. W is a dxdxk tensor.
+def bilinear_tensor_product(e1, W, e2):
+	return tf.matmul()
+
+
 
 
 # tf.matmul()
 # tf.batch_matmul()
 
 
-# def g((e1, R, e2)):
+W = tf.Variable([])
+V = tf.Variable([])
+b = tf.Variable([])
+
+
+
+def g((e1, R, e2)):
+
+
+	a = tf.add(bilinear_tensor_product(e1, W, e2), tf.matmul(V, tf.concat(0, [e1, e2])), b)
+
+	b = tf.tanh(a)
+
+	return tf.matmul(U, b)
+
 
 # These are just notes and ideas about functions we use
 
@@ -17,4 +36,6 @@ contrastive_max_margin = max(0, 1-g(true_triplet)+g(corrupt_triplet)) # + regula
 
 
 train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
+
+
 
