415a32acede32cddca4404e97f7d52ed2c552812
Dustin Doss <ddoss@mit.edu>
2015-12-09 00:59:52 -0500

    Add boilerplate and batch reaction code

--- code/ntn.py
+++ code/ntn.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import params
 import ntn_input
+import random
 
 # Inference
 # Loss
@@ -14,6 +15,7 @@ class NTN:
         self.num_relations  = hyperparameters['num_relations']
         self.batch_size     = hyperparameters['batch_size']
         self.k = k          = hyperparameters['slice_size']
+        self.corrupt_size   = hyperparameters['corrupt_size']
         # self.word_indices        = hyperparameters['word_indices']
         # self.activation_function = hyperparameters['activation_function']
         self.regularization = hyperparameters['lambda']
@@ -23,15 +25,20 @@ class NTN:
 
         data_path = hyperparameters['data_path']
         entities_strings = ntn_input.load_entities(data_path)
-        self.relations = tf.constant(ntn_input.load_relations(data_path))
+        self.entity_to_index = {entities_strings[i] : i for i in len(entities_strings)}
+        relation_strings = tf.constant(ntn_input.load_relations(data_path))
+        self.relation_to_index = {relation_strings[i] : i for i in len(relation_strings)}
+
         (initEmbeds, entity_words) = ntn_input.load_init_embeds(data_path) 
         
-        self.entity_words = entity_words #map from entity indices to word 
+        self.entity_words = entity_words #map from entity indices to list of word indices 
         self.W = W = dict()
         self.V = V = dict()
         self.b = b = dict()
         self.U = U = dict()
         self.E = E = tf.Variable(initEmbeds) #word embeddings
+
+        self.examples = tf.placeholder(tf.float32, shape=(4, self.batch_size), name='batch')
 
         for r in self.relations:
             W[r] = tf.Variable(tf.truncated_normal([d, d, k])) # W[i] = np.random.random([d, d, k]) * 2 * r - r
@@ -58,20 +65,38 @@ class NTN:
         temp = tf.matmul(U, temp)
         return temp
 
+    def L2(self):
+        term = 0
+        #sqrt(sum(trainable tensors))
+        return term
+
+    #randomly returns a batch_size*4 matrix: e1, R, e2, e3
+    #e3 is a random vector
+    def get_batch(self, batch_size, data):
+        random_indices = random.sample(range(len(data)), batch_size)
+        #data[i][0] = e1, data[i][1] = r, data[i][2] = e2, random=e3 (corrupted)
+        batch = [(data[i][0], data[i][1], data[i][2], random.randint(len(self.num_entities))) for i in random_indices for j in range(self.corrupt_size)]
+        return batch
+
+
+
     # LOSS
-    def loss(self, batch):
-        contrastive_max_margin = max(0, 1-g(true_triplet)+g(corrupt_triplet)) # + regularization term
+    def loss(self, batch, flip=True):        
+        contrastive_max_margin = max(0.0, 1.0-g(true_triplet)+g(corrupt_triplet)) # + regularization term
         train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
 
     def train(self):
+        training_strings = ntn_input.load_training_data()
+        training_data = [(self.entity_to_index(entry[0]), self.relation_to_index(entry[1]), self.entity_to_index(entry[2])) for entry in training_strings]
         with tf.Session() as sess:
             init = tf.initialize_all_variables()
             sess.run(init)
             for i in range(iterations):
-                sess.run(train_step, feed_dict={     })
+                batch = self.get_batch()
+                sess.run(train_step, feed_dict={'batch':batch})
 
 if __name__=="__main__":
-    hyperparameters = {"data_path":params.data_path,
+    hyperparameters = {"data_path": params.data_path,
             "num_iter":             params.num_iter,
             "train_both":           params.train_both,
             "batch_size":           params.batch_size,
