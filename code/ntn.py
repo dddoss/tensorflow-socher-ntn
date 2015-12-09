0b130cef57ea25c91320895496f87c18d83c3021
Alexander Lenail <alexander.lenail@tufts.edu>
2015-12-09 18:32:06 -0500

    remove bad things

--- code/ntn.py
+++ code/ntn.py
@@ -7,121 +7,43 @@ import random
 # Loss
 # Training
 
-<<<<<<< HEAD
-class NTN:
-    def __init__(self, hyperparameters):
-        # self.num_words           = hyperparameters['num_words']
-        self.d = d          = hyperparameters['embedding_size']
-        self.num_entities   = hyperparameters['num_entities']
-        self.num_relations  = hyperparameters['num_relations']
-        self.batch_size     = hyperparameters['batch_size']
-        self.k = k          = hyperparameters['slice_size']
-        # self.word_indices        = hyperparameters['word_indices']
-        self.regularization = hyperparameters['lambda']
-
-        # a 2D tensor with entity vectors. Someone needs to make this
-        # and somehow we need to be able to index into it
-        self.E = Entities()
-
-        for r in range(self.num_relations):
-            W[r] = tf.Variable(tf.truncated_normal([d, d, k])) # W[i] = np.random.random([d, d, k]) * 2 * r - r
-            V[r] = tf.Variable(tf.zeros([2 * d, k]))
-            b[r] = tf.Variable(tf.zeros([1, k]))
-            U[r] = tf.Variable(tf.ones([k, 1]))
-
-
-    # e1 and e2 are d-dimensional entity vectors. W is a dxdxk tensor.
-    def bilinearTensorProduct(self, e1, W, e2):
-        e1 = tf.reshape(e1, [1, d])
-        W = tf.reshape(W, [d, d*k])
-        temp = tf.matmul(e1, W)
-        temp = tf.reshape(temp, [k, d])
-        e2 = tf.reshape(e2, [d, 1])x
-        temp = tf.matmul(temp, e2)
-        return temp
-
-    def g(self, (e1, R, e2)):
-        temp1 = bilinearTensorProduct(e1, W, e2)
-        temp2 = tf.matmul(V, tf.concat(0, [e1, e2]))
-        temp = tf.add(temp1, temp2, b)
-        temp = tf.tanh(temp)
-        temp = tf.matmul(U, temp)
-        return temp
-
     # E1 and E2 are matrices of b columns of d-dim entity vectors. W is a dxdxk tensor.
-    def BATCH_bilinearTensorProduct(self, E1, W, E2):
+# def BATCH_bilinearTensorProduct(self, E1, W, E2):
 
-        e1 = tf.reshape(e1, [d, b, 1])
-        W = tf.reshape(W, [d*k, d])
+#     e1 = tf.reshape(e1, [d, b, 1])
+#     W = tf.reshape(W, [d*k, d])
 
-        temp = tf.matmul(e1, W)
-        temp = tf.reshape(temp, [k, d])
-        e2 = tf.reshape(e2, [d, 1])
-        temp = tf.matmul(temp, e2)
-        return temp
+#     temp = tf.matmul(e1, W)
+#     temp = tf.reshape(temp, [k, d])
+#     e2 = tf.reshape(e2, [d, 1])
+#     temp = tf.matmul(temp, e2)
+#     return temp
 
-    def BATCH_g(self, (e1, R, e2)):
+# def BATCH_g(self, (e1, R, e2)):
 
-        temp1 = bilinearTensorProduct(e1, W, e2)
-        temp2 = tf.matmul(V, tf.concat(0, [e1, e2]))
-        temp = tf.add(temp1, temp2, b)
-        temp = tf.tanh(temp)
-        temp = tf.matmul(U, temp)
-        return temp
-
-
+#     temp1 = bilinearTensorProduct(e1, W, e2)
+#     temp2 = tf.matmul(V, tf.concat(0, [e1, e2]))
+#     temp = tf.add(temp1, temp2, b)
+#     temp = tf.tanh(temp)
+#     temp = tf.matmul(U, temp)
+#     return temp
 
 
+# # LOSS
+# def loss(self, batch):
+#     contrastive_max_margin = max(0, 1-g(true_triplet)+g(corrupt_triplet)) # + regularization term
+#     train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
 
+# def train(self):
 
+#     with tf.Session() as sess:
+#     	init = tf.initialize_all_variables()
+#         sess.run(init)
 
+#         for i in range(iterations):
 
+	  #          sess.run(train_step, feed_dict={     })
 
-
-    # LOSS
-    def loss(self, batch):
-        contrastive_max_margin = max(0, 1-g(true_triplet)+g(corrupt_triplet)) # + regularization term
-        train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
-
-    def train(self):
-
-        with tf.Session() as sess:
-        	init = tf.initialize_all_variables()
-            sess.run(init)
-
-            for i in range(iterations):
-
- 	           sess.run(train_step, feed_dict={     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if name=="__main__":
-    hyperparameters = {"data_path":params.data_path,
-            "num_iter":params.num_iter,
-            "train_both":params.train_both,
-            "batch_size":params.batch_size,
-            "corrupt_size":params.corrupt_size,
-            "embedding_size":params.embedding_size,
-            "slice_size":params.slice_size,
-            "lambda":params.reg_parameter,
-            "in_tensor_keep_normal":params.in_tensor_keep_normal,
-            "save_per_iter":params.save_per_iter,
-            "gradient_checking":params.gradient_checking
-        }
-    ntn = NTN(hyperparameters)
-=======
 # e1 and e2 are d-dimensional entity vectors. W is a dxdxk tensor.
 def bilinearTensorProduct(e1, W, e2):
     e1 = tf.reshape(e1, [1, d])
@@ -146,7 +68,7 @@ def g((e1, R, e2)):
 #    return term
 
 # LOSS OLD
-#def loss(batch, flip=True):        
+#def loss(batch, flip=True):
 #    contrastive_max_margin = max(0.0, 1.0-g(true_triplet)+g(corrupt_triplet)) # + regularization term
 #    train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
 
@@ -163,19 +85,19 @@ def inference(batch_placeholder, corrupt_placeholder, init_word_embeds,\
     U = [tf.Variable(tf.ones([k, 1])) for r in range(len(num_relations))]
 
 
-    e1, R, e2, e3 = tf.split(1, 4, batch_placeholder) #TODO: should the split dimension be 0 or 1? 
+    e1, R, e2, e3 = tf.split(1, 4, batch_placeholder) #TODO: should the split dimension be 0 or 1?
     #convert entity word index reps to embeddings... how?
     e1v = tf.reduce_mean(tf.gather(E, e1), 0)
     e2v = tf.reduce_mean(tf.gather(E, e2), 0)
     e3v = tf.reduce_mean(tf.gather(E, e3), 0)
 
-    
+
     #e1v, e2v, e3v should be (batch_size * 100) tensors by now
     for r in range(num_relations):
         #get e1, e2, e3 cooresponding to indices where R[i] = r
         #TODO... uhh, stuff
 
-            
+
 def loss(infer_results):
     pass
 
@@ -184,4 +106,3 @@ def training(loss_results):
 
 def eval(infer_results):
     pass
->>>>>>> origin/master
