8cebe17cadd025b38ce43605deefc02095cb276c
Alexander Lenail <alexander.lenail@tufts.edu>
2015-12-09 21:51:02 -0500

    remove old code.

--- code/ntn.py
+++ code/ntn.py
@@ -30,31 +30,8 @@ import random
 
 # e1 and e2 are d-dimensional entity vectors. W is a dxdxk tensor.
 def bilinearTensorProduct(e1, W, e2):
-    e1 = tf.reshape(e1, [1, d])
-    W = tf.reshape(W, [d, d*k])
-    temp = tf.matmul(e1, W)
-    temp = tf.reshape(temp, [k, d])
-    e2 = tf.reshape(e2, [d, 1])
-    temp = tf.matmul(temp, e2)
-    return temp
 
-def g((e1, R, e2)):
-    temp1 = bilinearTensorProduct(e1, W, e2)
-    temp2 = tf.matmul(V, tf.concat(0, [e1, e2]))
-    temp = tf.add(temp1, temp2, b)
-    temp = tf.tanh(temp)
-    temp = tf.matmul(U, temp)
-    return temp
 
-#def L2():
-#    term = 0
-#    #sqrt(sum(trainable tensors))
-#    return term
-
-# LOSS OLD
-#def loss(batch, flip=True):
-#    contrastive_max_margin = max(0.0, 1.0-g(true_triplet)+g(corrupt_triplet)) # + regularization term
-#    train_step = tf.train.AdagradOptimizer(0.01).minimize(contrastive_max_margin)
 
 #returns a (batch_size*corrupt_size, 2) vector corresponding to [g(T^i), g(T_c^i)] for all i
 def inference(batch_placeholder, corrupt_placeholder, init_word_embeds,\
@@ -69,13 +46,6 @@ def inference(batch_placeholder, corrupt_placeholder, init_word_embeds,\
     b = [tf.Variable(tf.zeros([1, k])) for r in range(len(num_relations))]
     U = [tf.Variable(tf.ones([k, 1])) for r in range(len(num_relations))]
 
-
-    #ent2words = [[0]*num_words for i in range(num_entities)]
-    #for i in range(len(entity_to_wordvec)):
-    #    for j in entity_to_wordvec[i]:
-    #        ent2words[i][j] = 1.0/len(entity_to_wordvec[i])
-    #ent2words_tensor = tf.Constant(ent2words) #each row i cooresponds to entity i; e2w_tensor[i]*E=entity embedding
-    
     #python list of tf vectors: i -> list of word indices cooresponding to entity i
     ent2word = [tf.constant(entity_to_wordvec[i]) for i in range(num_entities)]
     #(num_entities, d) matrix where row i cooresponds to the entity embedding (word embedding average) of entity i
@@ -96,7 +66,34 @@ def inference(batch_placeholder, corrupt_placeholder, init_word_embeds,\
     #e1v, e2v, e3v should be (batch_size * 100) tensors by now
     for r in range(num_relations):
         #calc g(e1, R, e2) and g(e1, R, e3) for each relation
-        predictions.append(tf.pack([g(e1r[r],  W[r], e2r[r]), g(e1r[r], W[r], e3r[r])]))
+        # predictions.append(tf.pack([g(e1r[r],  W[r], e2r[r]), g(e1r[r], W[r], e3r[r])]))
+
+        for slice in range(k):
+
+            # preactivation_pos[slice, :] = np.sum(entity_vectors_e1     * np.dot(W[i][:, :, slice], entity_vectors_e2), axis = 0)
+
+
+
+          tf.matmul() W[r]
+
+        # bilinear tensor product:
+        e1 = tf.reshape(e1, [1, d])
+        Wr = tf.reshape(W, [d, d*k])
+        temp = tf.matmul(e1, W)
+        temp = tf.reshape(temp, [k, d])
+        e2 = tf.reshape(e2, [d, 1])
+        temp1 = tf.matmul(temp, e2)
+
+        # neural tensor layer:
+        temp2 = tf.matmul(V, tf.concat(0, [e1, e2]))
+        temp = tf.add(temp1, temp2, b)
+        temp = tf.tanh(temp)
+        temp = tf.matmul(U, temp)
+        return temp
+
+
+
+
 
     predictions = tf.pack(predictions)
 
@@ -121,7 +118,7 @@ def training(loss, learningRate):
 
 
 def eval(predictions):
-
+    pass
 
 
 
